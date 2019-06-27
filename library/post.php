fc874c49356b55af6aa78b2039a277e3ec234d24
agungmohmd <mohmd.agung@gmail.com>
2019-06-27 16:59:38 +0700

    pengerjaan post lanjutan (belum siap)

--- library/post.php
+++ library/post.php
@@ -37,5 +37,42 @@ if (member_valid($post['username'], $post['password'])) {
     echo $result;
 }
 
-// next code here ...
+elseif ($post['aksi'] == "single") {
+  $data = array();
+  $query = mysqli_query($mysqli, "SELECT * FROM post LEFT JOIN member ON post.id_member=member.id_member WHERE post.id_post='$post[idpost]'");
+  $row = mysqli_fetch_array($query);
+
+  $jml_like = mysqli_num_rows(mysqli_query($mysqli, "SELECT * FROM post_like WHERE id_post = '$row[idpost]' AND id_member = '$post[member]'"));
+
+  $jml_comment = mysqli_num_rows(mysqli_query($mysqli, "SELECT * FROM comment WHERE id_post='$row[id_post]'"));
+  $data[] = array(
+    'id' => $row['id_post'],
+    'id_member' => $row['id_member'],
+    'foto' => $row['photo'],
+    'nama' => $row['name'],
+    'post' => $row['post'],
+    'gambar' => $row['image'],
+    'suka' => $jml_like,
+    'jml_komentar' => $jml_comment,
+    'tanggal' => tgl_indonesia($row['created_at'])  
+  );
+
+  if($query) $result = json_encode(array('success'=>true, 'result'=>$data));
+  else $result = json_encode(array('success'=>false));
+  echo $result;
+}
+
+elseif($post['aksi'] == "profil"){
+  $data = array();
+  $query = mysqli_query($mysqli, "SELECT * FROM post WHERE id_member='$post[target]' ORDER BY id_post DESC");
+  while ($row = mysqli_fetch_array($query)) {
+    $data[] = array(
+      'id' => $row['id_post'],
+      'gambar' => $row['image']
+    );
+  }
+
+  // next code here ///
+}
+
 ?>
